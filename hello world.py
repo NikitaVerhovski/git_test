6d0b61c9d382e22b59645d6a857e83820e05eaf2
NikitaVerhovski <46034874+NikitaVerhovski@users.noreply.github.com>
2019-01-09 22:22:32 +0300

    Создал файл Hello world и в нем использовал функцию формат

---
+++ hello world.py
@@ -0,0 +1,15 @@
+print("Hello world")
+auto = "bmw".upper()
+auto1 = "audi".upper()
+auto2 = "mazda".upper()
+auto3 = "honda".upper()
+auto4 = "toyota".upper()
+auto5 = "ford".upper()
+auto6 = "porsche".upper()
+print("Я хочу иметь в своем гараже семь машин : {}, {}, {}, {}, {}, {}, {}".format(auto, auto1, auto2, auto3, auto4, auto5, auto6))
+
+
+
+
+
+
