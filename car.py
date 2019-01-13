0cc1e384216eac78197dfc0bebc4f97103fdfda3
NikitaVerhovski <46034874+NikitaVerhovski@users.noreply.github.com>
2019-01-14 00:10:32 +0300

    Merge branch 'list/bike'

--- car.py
+++ car.py
@@ -1,5 +1,7 @@
 cars = ["honda", "toyota", "suzuki", "lada", "bmw", "audi"]
-print("Я всегда хотел иметь в своем гараже; : ", cars[0], ",", cars[1])
+print("Я всегда хотел иметь в своем гараже;  : ", cars[0], ",", cars[1])
+print("А вот такую машину как ", cars[3], "никогда !!")
 bike = ["ducati", "yamaha", "honda"]
 print("Мой любимый мотоцикл ;", bike[0], "!")
+bike.append("kawasaki")
 
